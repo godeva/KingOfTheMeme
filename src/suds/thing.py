4e293999f0199346ad6ba76d4e2c8fe7905bb1ae
Aaron Thomas <1d20m6@gmail.com>
2017-02-01 17:29:09 -0500

    Added Inventory class
    Various Things (namely Entities, Rooms, and various Items) will have
    other items stored inside of them. Each of these classes should have an
    Inventory rather than their own respective implementation of this
    functionality.

--- src/suds/thing.py
+++ src/suds/thing.py
@@ -7,3 +7,34 @@ class Thing:
 	def __init__(self, name="Generic Thing", description="If you are seeing this then there was probably a bug."):
 		self.name = name
 		self.description = description
+
+class Inventory:
+	'''
+	Might just get its own file in the future
+	'''
+	def __init__(self,max_size=0, items=[]):
+		self.max_size = max_size
+		self.items = items
+
+	def empty_slots(self):
+		'''
+		returns how many empty slots are in the inventory
+		a negative number indicates how many items are in the inventory over the maximum amount
+		'''
+		return max_size - len(items)
+
+	def is_full(self):
+		'''
+		returns true if the inventory is full of items or is overflowing
+		returns false otherwise
+		'''
+		return max_size >= len(items)
+
+	def add_item(self, item):
+		'''
+		@params item is an item that is being added to the inventory
+		'''
+		if not self.is_full(): #todo(aaron) add an isinstance() call
+			items.append(item)
+			return None
+		return item
