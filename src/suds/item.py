20dddab2eb0d48300f42b84f291e8c91f5cf2206
Aaron Thomas <1d20m6@gmail.com>
2017-02-01 17:28:13 -0500

    Added skeleton Container class

--- src/suds/item.py
+++ src/suds/item.py
@@ -3,3 +3,7 @@ import thing
 class Item(Thing):
 	def __init__(self, name, desc, NEEDS_MORE_PARAMS):
 		pass #@todo(aaon) code this
+
+class Container(Item):#an item that you can put other items in!
+	def __init__(self, name, desc, inv, NEEDS_MORE_PARAMS):
+		pass #@todo(aaron) design theitem system before implementing it
